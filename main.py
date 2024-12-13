8ffe1c59be02977c3176581c71f12b89ac2fe843
hiiion <141587325+hiiion@users.noreply.github.com>
2024-12-13 14:35:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, File, UploadFile, Form, HTTPException
-from typing import Optional
+from fastapi import FastAPI, File, Form, UploadFile
 import os
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel, HttpUrl, Field
+from typing import List, Optional
+from fastapi.responses import JSONResponse
 
 # FastAPI 인스턴스 생성
 app = FastAPI()
@@ -13,6 +15,16 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+class HowToStep(BaseModel):
+    type: str = Field(..., alias='@type')  # Use alias to map @type to type
+    text: str
+    image: str
+
+class Recipe(BaseModel):
+    name: str
+    recipeIngredient: List[str] # Map 'ingredients' from JSON
+    recipeInstructions: List[HowToStep]  # Map 'instructions' from JSON
 
 # 파일 저장 경로 설정
 UPLOAD_DIRECTORY = "./uploaded_files"
@@ -27,17 +39,22 @@ async def upload_file(file: UploadFile = File(...)):
         return {"filename": file.filename, "message": "File uploaded successfully!"}
     except Exception as e:
         return {"error": str(e)}
+    
+@app.post("/send-recipe")
+async def send_recipe(recipe: Recipe):
+    print(recipe)  # Add this line to log the received data
+    return JSONResponse(content={"message": "Recipe successfully received", "recipe": recipe.dict()})
 
 '''
 @app.post("/demo/")
-async def demo(step: int = Form(...)):
+async def demo():
     try:
         # demo.py의 AI 모듈을 호출하여 결과 반환 (가정)
         # 실제 demo.py의 함수 호출 로직으로 대체 필요
         from demo import get_next_step_info  # demo.py 모듈에서 함수 가져오기
 
         # get_next_step_info는 step 정보를 받아서 결과 반환한다고 가정
-        demo_results = get_next_step_info(step)
+        demo_results = get_next_step_info()
 
         # 결과 형태는 {'nextStep': bool, 'caution': Optional[str], 'timer': Optional[int], "responseAudio": Optional[str]}로 가정
         return demo_results
